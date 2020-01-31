2270409ba1bb4066ad10ea3161945e9431c79fb9
r4d6 <david.moussette@gmail.com>
2020-01-31 17:05:59 -0500

    Adding Intro OO

---
+++ Intro OO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro_OO
+{
+    class Program
+    {
+        // Le paramètre "args" (tableau de chaine de charactère) va servier à recevoir des arguments lors du lancement du programme
+        // Exemples à venir plus tard
+        static void Main(string[] args)
+        {
+        }
+    }
+}

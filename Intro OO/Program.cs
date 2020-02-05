4b8f12dbdbf5716a767ccb249f80628454916ffe
r4d6 <david.moussette@gmail.com>
2020-02-05 11:32:58 -0500

    Rectangles

--- Intro OO/Program.cs
+++ Intro OO/Program.cs
@@ -12,6 +12,36 @@ namespace Intro_OO
         // Exemples à venir plus tard
         static void Main(string[] args)
         {
+            Rectangle r1 = new Rectangle();
+
+            r1.SetHauteur(4);
+            r1.Largeur = 6;
+            r1.Nom = "Aelith";
+
+            Console.WriteLine(r1.Nom + ": Largeur = " + r1.Largeur + ", Hauteur = " + r1.Hauteur() + ", Aire = " + r1.Air());
+            Console.WriteLine();
+            Console.WriteLine();
+            r1.Afficher();
+
+
+            Rectangle r2 = new Rectangle();
+
+            r2.SetHauteur(8);
+            r2.Largeur = 12;
+            r2.Nom = "Aelita";
+
+            Console.WriteLine(r2.Nom + ": Largeur = " + r2.Largeur + ", Hauteur = " + r2.Hauteur() + ", Aire = " + r2.Air());
+            Console.WriteLine();
+            Console.WriteLine();
+            r2.Afficher('+');
+
+            Pause();
+        }
+
+        static void Pause()
+        {
+            Console.WriteLine("Appuyer sur une touche pour continuer");
+            Console.ReadKey(true);
         }
     }
 }
---
+++ Intro OO/Rectangle.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro_OO
+{
+    /// <summary>
+    /// Representation d'une forme géométrique de type rectangle
+    /// </summary>
+    class Rectangle
+    {
+        // Par defaut, le contenu d'une classe est privé, c'est-à-dire qu'il est impossible d'y accéder de l'extérieur
+        // Le mot clé "public" indique que ce qui suit peut être accéder de l'extérieur
+
+        // Toutes les définitions seont mises en premier dans la classe, car elles intéressent les utilisateurs
+        // Tous ce qui est privé est mis à la fin, car ça n'intéresse que le concepteur de la classe
+
+        // Un constructeur est une méthode spécial qui est appelé automatiquement lors de la création d'un objet
+        // Un constructeur n'a pas un type de retour
+        // Un constructer porte exactement le même nom que la classe
+        public Rectangle()
+        {
+
+        }
+
+        // Pour respecter le principe d'encapsulation des données, aucun attribut ne sera public, il faut donc fournir des méthodes 
+        // Une méthode pour aller chercer la valeur d'un attribut se nomme un "accesseur" (getter)
+        public int Hauteur()
+        {
+            return _hauteur;
+        }
+        
+
+        // Une méthode qui permet de modifier un attribut se nomme un "mutateur" (setter)
+        public void SetHauteur(int value)
+        {
+            // Un mutateur va permettre de valider les valeurs donnés avant de les assigner
+            // La hauteur ne peut pas être négative ni null
+            if (value > 0)
+            {
+                _hauteur = value;
+            }
+        }
+
+        // Une propriété est à mi-chemin entre un attribut et une méthode
+        // S'utilise comme un attribut, se comporte comme une méthode
+        // Pas de ()
+        public int Largeur
+        {
+            // Dans le bloc d'une propriété, il est possible de définir 2 simili-méthodes
+
+            // Accesseur
+            get { return _largeur; }
+
+            // Mutateur
+            set
+            {
+                // Dans la partie set d'une propriété, une variable nommé "value" existe implicitement pour contenir la valeur assigné
+                if (value > 0)
+                {
+                    _largeur = value;
+                }
+            }
+        }
+
+        // Propriété automatique
+        // Une valeur interne est utilisé implicitemen, sans qu'on doive definir un attribut _nom
+        // le get retourne simplement cette valeur, le set l'assigne sans validation
+        // Il est possible d'assigner une valeur par défaut à la propriété
+        // !!! À utiliser avec modération!, n'est qu'une très légère couche au dessus d'un attribut afin que celui-ci reste privé
+        public string Nom { get; set; }
+
+
+        //Propriété automatique, en lecture seule, mais modifiable dans la classe
+        public int Couleur { get; private set; } = 15;
+
+        //Une méthode qui retourne l'air du rectangle
+        public int Air() { return _largeur * _hauteur; }
+
+        // Propriété pour récupérer l'aire du rectangle
+        // Propriété en lecture seule
+        public int Aire
+        {
+            get { return _largeur * _hauteur; }
+            // On ne définie pas la méthode set
+        }
+
+        /// <summary>
+        /// Dessine le rectangle à l'écran avec le symbole donné
+        /// </summary>
+        /// <param name="symbole"></param>
+        public void Afficher(char symbole)
+        {
+            // Comme la méthode
+             for (int ligne = 0; ligne < _hauteur; ligne++)
+            {
+                for (int colonne = 0; colonne < _largeur; colonne++)
+                {
+                    Console.Write(symbole);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // Il est possible d'avoir plusieus méthodes qui porte le même nom tant que le compilateur est capable de faire la difference en se basant sur les paramètres
+        public void Afficher()
+        {
+            // Appele la version avec des paramètres
+            Afficher('*');
+        }
+
+        // Une variable définie dans une class se nomme un attribut
+        // Il n'y aura JAMAIS d'attribut public dans une classe
+        // Même si par défaut l'accès est privé, nous allons explicitement le mentionner pour éviter la confusion
+        // Le mot clé "private" indique que c'est privé à la classe
+        private int _hauteur = 0;
+        private int _largeur = 0;
+    }
+}

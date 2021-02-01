20c3a6e5f076741552e070bc746fef70ca81f69d
r4d6 <david.moussettegmail.com>
2021-02-01 10:44:15 -0500

    .

---
+++ CSharp/CSharp/CSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System; // Use that to not have to put 'System.' in front of every command
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press a key to continue");
+            Console.ReadKey();
+            Console.Clear();
+            BaseTest();
+        }
+
+        static void BaseTest()
+        {
+            Console.WriteLine("Hello everyone!");
+            Console.WriteLine("Press a key to continue");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Welcome to Programmation Class.");
+            Console.WriteLine("Press a key to continue");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
